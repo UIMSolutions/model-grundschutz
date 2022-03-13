adba0ee8ba07edfd2e47855811614784d10c8d6a
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-03-13 16:55:25 +0100

    Uodates

--- source/models/bsi/entities/systems/system.d
+++ source/models/bsi/entities/systems/system.d
@@ -8,57 +8,50 @@ static this() {
   createEntities["bsiSystem"] = (Json json) => BSISystem(json); 
 }
 // 
-  class DBSISystem : DOOPEntity {
-  this() { super();
-    this.attributes([
-      "projectId":`{"datatype":"UUID", "descriptions":{"en":"Unique identifier of the project with which this entity is associated."}}`,
-      "platformId":`{"datatype":"UUID", "descriptions":{"en":"Unique identifier of the platform with which this entity is associated."}}`,
-      "confidentialityCode":`{"datatype":"String", "descriptions":{"en":""}}`,
-      "confidentialityReason":`{"datatype":"String", "descriptions":{"en":""}}`,
-      "integrityCode":`{"datatype":"String", "descriptions":{"en":""}}`,
-      "integrityReason":`{"datatype":"String", "descriptions":{"en":""}}`,
-      "availabilityCode":`{"datatype":"String", "descriptions":{"en":""}}`,
-      "availabilityReason":`{"datatype":"String", "descriptions":{"en":""}}`,
-      /*"annotationId":`{"datatype":"UUID", "descriptions":{"en":"Unique identifier of the note."}}`,
-      "objectTypeCode":`{"datatype":"Integer", "descriptions":{"en":"Type of entity with which the note is associated."}}`,
-      "objectTypeCode_display":`{"datatype":"String", "descriptions":{"en":""}}`,
-      "owningUserId":`{"datatype":"UUID", "descriptions":{"en":"Unique identifier of the user who owns the note."}}`,
-      "objectId":`{"datatype":"UUID", "descriptions":{"en":"Unique identifier of the object with which the note is associated."}}`,
-      "objectIdTypeCode":`{"datatype":"Integer", "descriptions":{"en":"The name of the entity linked by objectId"}}`,
-      "owningBusinessUnit":`{"datatype":"String", "descriptions":{"en":"Unique identifier of the business unit that owns the note."}}`,
-      "subject":`{"datatype":"String", "descriptions":{"en":"Subject associated with the note."}}`,
-      "isDocument":`{"datatype":"String", "descriptions":{"en":"Specifies whether the note is an attachment."}}`,
-      "noteText":`{"datatype":"String", "descriptions":{"en":"Text of the note."}}`,
-      "mimeType":`{"datatype":"String", "descriptions":{"en":"MIME type of the note's attachment."}}`,
-      "langId":`{"datatype":"UUID", "descriptions":{"en":"Language identifier for the note."}}`,
-      "documentBody":`{"datatype":"String", "descriptions":{"en":"Contents of the note's attachment."}}`,
-      "fileSize":`{"datatype":"String", "descriptions":{"en":"File size of the note."}}`,
-      "fileName":`{"datatype":"String", "descriptions":{"en":"File name of the note."}}`,
-      "ownerId":`{"datatype":"UUID", "descriptions":{"en":"Owner Id"}}`,
-      "ownerIdType":`{"datatype":"String", "descriptions":{"en":"The type of owner, either User or Team."}}`,
-      "stepId":`{"datatype":"UUID", "descriptions":{"en":"workflow step id associated with the note."}}`,
-      "overriddenCreatedOn":`{"datatype":"DateTime", "descriptions":{"en":"Date and time that the record was migrated."}}`,
-      "importSequenceNumber":`{"datatype":"String", "descriptions":{"en":"Unique identifier of the data import or data migration that created this record."}}`,
-      "createdOnBehalfBy":`{"datatype":"UserId", "descriptions":{"en":"Unique identifier of the delegate user who created the annotation."}}`,
-      "modifiedOnBehalfBy":`{"datatype":"UserId", "descriptions":{"en":"Unique identifier of the delegate user who last modified the annotation."}}`,
-      "owningTeamId":`{"datatype":"UUID", "descriptions":{"en":"Unique identifier of the team who owns the note."}}`, */
-    ]);
+class DBSISystem : DOOPEntity {
+  mixin(OOPEntityThis!("BSISystem"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addAttributes([
+				"projectId":`{"datatype":"UUID", "descriptions":{"en":"Unique identifier of the project with which this entity is associated."}}`,
+				"platformId":`{"datatype":"UUID", "descriptions":{"en":"Unique identifier of the platform with which this entity is associated."}}`,
+				"confidentialityCode":`{"datatype":"String", "descriptions":{"en":""}}`,
+				"confidentialityReason":`{"datatype":"String", "descriptions":{"en":""}}`,
+				"integrityCode":`{"datatype":"String", "descriptions":{"en":""}}`,
+				"integrityReason":`{"datatype":"String", "descriptions":{"en":""}}`,
+				"availabilityCode":`{"datatype":"String", "descriptions":{"en":""}}`,
+				"availabilityReason":`{"datatype":"String", "descriptions":{"en":""}}`,
+				/*"annotationId":`{"datatype":"UUID", "descriptions":{"en":"Unique identifier of the note."}}`,
+				"objectTypeCode":`{"datatype":"Integer", "descriptions":{"en":"Type of entity with which the note is associated."}}`,
+				"objectTypeCode_display":`{"datatype":"String", "descriptions":{"en":""}}`,
+				"owningUserId":`{"datatype":"UUID", "descriptions":{"en":"Unique identifier of the user who owns the note."}}`,
+				"objectId":`{"datatype":"UUID", "descriptions":{"en":"Unique identifier of the object with which the note is associated."}}`,
+				"objectIdTypeCode":`{"datatype":"Integer", "descriptions":{"en":"The name of the entity linked by objectId"}}`,
+				"owningBusinessUnit":`{"datatype":"String", "descriptions":{"en":"Unique identifier of the business unit that owns the note."}}`,
+				"subject":`{"datatype":"String", "descriptions":{"en":"Subject associated with the note."}}`,
+				"isDocument":`{"datatype":"String", "descriptions":{"en":"Specifies whether the note is an attachment."}}`,
+				"noteText":`{"datatype":"String", "descriptions":{"en":"Text of the note."}}`,
+				"mimeType":`{"datatype":"String", "descriptions":{"en":"MIME type of the note's attachment."}}`,
+				"langId":`{"datatype":"UUID", "descriptions":{"en":"Language identifier for the note."}}`,
+				"documentBody":`{"datatype":"String", "descriptions":{"en":"Contents of the note's attachment."}}`,
+				"fileSize":`{"datatype":"String", "descriptions":{"en":"File size of the note."}}`,
+				"fileName":`{"datatype":"String", "descriptions":{"en":"File name of the note."}}`,
+				"ownerId":`{"datatype":"UUID", "descriptions":{"en":"Owner Id"}}`,
+				"ownerIdType":`{"datatype":"String", "descriptions":{"en":"The type of owner, either User or Team."}}`,
+				"stepId":`{"datatype":"UUID", "descriptions":{"en":"workflow step id associated with the note."}}`,
+				"overriddenCreatedOn":`{"datatype":"DateTime", "descriptions":{"en":"Date and time that the record was migrated."}}`,
+				"importSequenceNumber":`{"datatype":"String", "descriptions":{"en":"Unique identifier of the data import or data migration that created this record."}}`,
+				"createdOnBehalfBy":`{"datatype":"UserId", "descriptions":{"en":"Unique identifier of the delegate user who created the annotation."}}`,
+				"modifiedOnBehalfBy":`{"datatype":"UserId", "descriptions":{"en":"Unique identifier of the delegate user who last modified the annotation."}}`,
+				"owningTeamId":`{"datatype":"UUID", "descriptions":{"en":"Unique identifier of the team who owns the note."}}`, */
+      ])
+      .registerPath("bsi_systems");
   }
-
-  override string entityClass() { return "bsiSystem"; }
-  override string entityClasses() { return "bsiSystems"; }
-
-  this(UUID myId) { 
-    this(); this.id(myId); }
-  this(string myName) { 
-    this(); this.name(myName); }
-  this(UUID myId, string myName) { 
-    this(); this.id(myId).name(myName); }
-  this(Json aJson) { 
-    this(); this.fromJson(aJson); }
 }
-auto BSISystem() { return new DBSISystem; } 
-auto BSISystem(Json json) { return new DBSISystem(json); } 
+mixin(OOPEntityCalls!("BSISystem"));
 
 unittest {
   version(uim_entities) {
