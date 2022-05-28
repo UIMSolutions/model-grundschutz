266ee9378959619b6deaf6a6a668a30a54674d9a
UIM (Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-05-28 17:57:17 +0200

    Update process.d

--- source/models/bsi/entities/processes/process.d
+++ source/models/bsi/entities/processes/process.d
@@ -1,4 +1,4 @@
-module models.bsi.processes.process;
+module models.bsi.entities.processes.process;
 
 @safe:
 import models.bsi;
@@ -46,12 +46,5 @@ version(test_library) {
     assert(BSIProcess);
   
   auto entity = BSIProcess;
-  // auto repository = OOPFileRepository("./tests");
-/*  repository.create("entities", entity.entityClasses, entity.toJson);
-
-  auto json = repository.findOne("entities", entity.entityClasses, ["id":entity.id.toString]);
-  assert(json != Json(null), entity.id.toString~" not found");
-
-  repository.cleanupConnections; */
 	}
 }
