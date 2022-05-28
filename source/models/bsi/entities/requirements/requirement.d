188ce1c104240fe7d56a470ae30ce361d467861a
UIM (Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-05-28 15:06:52 +0200

    Update requirement.d

--- source/models/bsi/entities/requirements/requirement.d
+++ source/models/bsi/entities/requirements/requirement.d
@@ -1,4 +1,4 @@
-module models.bsi.requirements.requirement;
+module models.bsi.entities.requirements.requirement;
 
 @safe:
 import models.bsi;
@@ -46,12 +46,5 @@ version(test_library) {
     assert(BSIRequirement);
   
   auto entity = BSIRequirement;
-  // auto repository = OOPFileRepository("./tests");
-/*  repository.create("entities", entity.entityClasses, entity.toJson);
-
-  auto json = repository.findOne("entities", entity.entityClasses, ["id":entity.id.toString]);
-  assert(json != Json(null), entity.id.toString~" not found");
-
-  repository.cleanupConnections; */
 	}
 }
