d941a07dc8cdd6db67d9d3cc9567ae8dca792aa9
UIM (Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-05-28 12:53:35 +0200

    Update package.d

--- source/models/bsi/entities/documents/package.d
+++ source/models/bsi/entities/documents/package.d
@@ -1,11 +1,11 @@
-module models.bsi.documents;
+module models.bsi.entities.documents;
 
 // Main entity
-public import models.bsi.documents.document;
+public import models.bsi.entities.documents.document;
 
 // Additional entities
-public import models.bsi.documents.category;
-public import models.bsi.documents.link;
-public import models.bsi.documents.linkroot;
-public import models.bsi.documents.reference;
+public import models.bsi.entities.documents.category;
+public import models.bsi.entities.documents.link;
+public import models.bsi.entities.documents.linkroot;
+public import models.bsi.entities.documents.reference;
 
