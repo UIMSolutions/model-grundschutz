368ed6e9903ab27c4e45eafdfbb43d440f1194e6
UIM (Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-05-29 07:24:38 +0200

    Update package.d

--- source/models/bsi/entities/package.d
+++ source/models/bsi/entities/package.d
@@ -2,3 +2,7 @@ module models.bsi.entities;
 
 @safe:
 import models.bsi;
+
+public import models.bsi.entities.applications;
+public import models.bsi.entities.systems;
+public import models.bsi.entities.users;
