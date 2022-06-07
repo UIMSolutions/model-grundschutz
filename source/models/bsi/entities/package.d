d0db113b080034d10842e0e472bc1f98dd79871e
UIM (Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-07 21:58:19 +0200

    Update package.d

--- source/models/bsi/entities/package.d
+++ source/models/bsi/entities/package.d
@@ -6,5 +6,8 @@ import models.grundschutz;
 public import models.grundschutz.entities.applications;
 public import models.grundschutz.entities.building;
 public import models.grundschutz.entities.networks;
+public import models.grundschutz.entities.persons;
+public import models.grundschutz.entities.platforms;
+public import models.grundschutz.entities.processes;
 public import models.grundschutz.entities.systems;
 public import models.grundschutz.entities.users;
