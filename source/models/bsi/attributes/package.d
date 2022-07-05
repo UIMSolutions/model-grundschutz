abd43295875f1f8787a13e091cbd486fdbd316a2
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-05 06:33:03 +0200

    Update package.d

--- source/models/bsi/attributes/package.d
+++ source/models/bsi/attributes/package.d
@@ -1,3 +1,8 @@
 module models.grundschutz.attributes;
 
+public import models.grundschutz.attributes.codes;
+public import models.grundschutz.attributes.elements;
+public import models.grundschutz.attributes.entites;
+public import models.grundschutz.attributes.functions;
+public import models.grundschutz.attributes.relationships;
 public import models.grundschutz.attributes.uuids;
